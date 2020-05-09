ccba79a078a92055fdf1ccb4c5448c28bdcb557f
Ruchira Prasad <RuchiraP@99x.lk>
2020-05-09 22:50:32 +0530

    expand button implementation

--- Micro.Fin/Fin-Web/src/app/shared/splitter/splitter-container/splitter-container.component.ts
+++ Micro.Fin/Fin-Web/src/app/shared/splitter/splitter-container/splitter-container.component.ts
@@ -11,11 +11,31 @@ export class SplitterContainerComponent implements OnInit {
   @ViewChild('panelleft', { read: ViewContainerRef, static: true }) panelLeftContainerRef;
   @ViewChild('panelright', { read: ViewContainerRef, static: true }) panelRightContainerRef;
   
+  shadesEl: any;
   constructor(private splitterService : SplitterService) { }
 
   ngOnInit() {
     this.splitterService.initializePanel(this.panelLeftContainerRef, this.panelRightContainerRef);
+    this.shadesEl = document.querySelector('.right-panel');
   }
   
-
+  toggleRightPanel(event){
+    if (!event.target.classList.contains('expand-btn')) {
+      event.target.classList.remove('collapse-btn');
+      event.target.classList.add('expand-btn');
+    }
+    else{
+      event.target.classList.remove('expand-btn');
+      event.target.classList.add('collapse-btn');
+    }
+    
+    if (!this.shadesEl.classList.contains('expand')) {
+      this.shadesEl.classList.remove('collapse');
+      this.shadesEl.classList.add('expand');
+    }
+    else{
+      this.shadesEl.classList.remove('expand');
+      this.shadesEl.classList.add('collapse');
+    }
+  }
 }
